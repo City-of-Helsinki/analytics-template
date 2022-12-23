29d82aa1420cf4cbf36e6b0a8adb56d5645dbd2b
manujoha <manu.johansson@vincit.fi>
2022-12-23 15:36:26 +0000

    Default docker-compose to jupyterlab.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 MODE=${MODE:-dev}
-PASS=${JUPYTER_LAB_PASSWORD:'h19eASLha98dfuj0as87'}
+
 if [[ $MODE = dev ]]
 then
     tail -f /dev/null
 
 elif [[ $MODE = jupyterlab ]]
 then
-    jupyter-lab --allow-root --ip 0.0.0.0 --port 8888. --password=$PASS
+    jupyter-lab --allow-root --ip 0.0.0.0 --port 8888
     
 else
     echo "unknown mode: "$MODE", use 'dev', 'jupyterlab' or leave empty (defaults to 'dev')"
