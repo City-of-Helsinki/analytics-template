fcd3c7fa97688ac62f27e7fbb5df6e1416af3b2c
manu <manu>
2023-02-07 12:45:08 +0200

    Added notebook cleaning check.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,16 @@ then
       echo "Run nbdev_test"
       cd /app
       nbdev_test
+
+      echo "Trying to strip out notebooks"
+      nbdev_clean
+      echo "Check that strip out was unnecessary"
+      git status -s # display the status to see which nbs need cleaning up
+      if [[ `git status --porcelain -uno` ]]; then
+        git status -uno
+        echo -e "!!! Detected unstripped out notebooks\n!!!Remember to install nbdev hooks to your environment using nbdev_install_hooks or execute nbdev_clean before commit."
+        false
+      fi
 else
     echo "unknown mode: "$MODE", use 'dev', 'jupyterlab' or leave empty (defaults to 'dev')"
 fi
