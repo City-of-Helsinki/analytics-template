634e357cbea65ed5804c20f4997bbbacbca51780
manu <manu>
2023-02-07 10:46:47 +0200

    Run nbdev_test in app folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,15 @@ MODE=${MODE:-dev}
 if [[ $MODE = dev ]]
 then
     tail -f /dev/null
-elif [[ $MODE = ci-test ]]
-then
-      echo "Run nbdev_test"
-      # nbdev_test --flags "$FLAGS"
-      nbdev_test
 elif [[ $MODE = jupyterlab ]]
 then
     jupyter-lab --allow-root --ip 0.0.0.0 --port 8888
+elif [[ $MODE = ci-test ]]
+then
+      # Declare github ci test procedures here
+      echo "Run nbdev_test"
+      cd /app
+      nbdev_test
 else
     echo "unknown mode: "$MODE", use 'dev', 'jupyterlab' or leave empty (defaults to 'dev')"
 fi
